fe22cadc71067cd03385751b561fe3b8cbceb86d
macMini <diegopandolfo06@gmail.com>
2026-05-18 11:39:46 +0200

    Add SleepMode enum and calculateSleepNow() to SleepViewModel

--- SleepWell/SleepWell/ViewModels/SleepViewModel.swift
+++ SleepWell/SleepWell/ViewModels/SleepViewModel.swift
@@ -2,25 +2,41 @@ import Foundation
 import Observation
 import SwiftUI
 
+enum SleepMode {
+    case wakeUp, sleepNow
+}
+
 @Observable
 final class SleepViewModel {
     var wakeTime: Date = defaultWakeTime()
     var bedtimes: [BedtimeOption] = []
     var showResults: Bool = false
     var selectedOption: BedtimeOption? = nil
+    var mode: SleepMode = .wakeUp
 
     @ObservationIgnored
     @AppStorage("fallAsleepMinutes") var fallAsleepMinutes: Int = 14
 
     func calculate() {
-        bedtimes = SleepCalculator.calculate(wakeTime: wakeTime, fallAsleepMinutes: fallAsleepMinutes)
+        switch mode {
+        case .wakeUp:
+            bedtimes = SleepCalculator.calculate(wakeTime: wakeTime, fallAsleepMinutes: fallAsleepMinutes)
+        case .sleepNow:
+            bedtimes = SleepCalculator.calculateWakeTimes(sleepTime: Date(), fallAsleepMinutes: fallAsleepMinutes)
+        }
         showResults = true
+    }
+
+    func calculateSleepNow() {
+        mode = .sleepNow
+        calculate()
     }
 
     func reset() {
         bedtimes = []
         showResults = false
         selectedOption = nil
+        mode = .wakeUp
     }
 }
 
