65a53c809542c3b001615a8d34a6a17ff78e8ea3
macMini <diegopandolfo06@gmail.com>
2026-05-18 10:05:13 +0200

    Add SleepViewModel

---
+++ SleepWell/ViewModels/SleepViewModel.swift
@@ -0,0 +1,28 @@
+import Foundation
+import Observation
+
+@Observable
+final class SleepViewModel {
+    var wakeTime: Date = defaultWakeTime()
+    var bedtimes: [BedtimeOption] = []
+    var showResults: Bool = false
+    var selectedOption: BedtimeOption? = nil
+
+    func calculate() {
+        bedtimes = SleepCalculator.calculate(wakeTime: wakeTime)
+        showResults = true
+    }
+
+    func reset() {
+        bedtimes = []
+        showResults = false
+        selectedOption = nil
+    }
+}
+
+private func defaultWakeTime() -> Date {
+    var components = Calendar.current.dateComponents([.year, .month, .day], from: Date())
+    components.hour = 7
+    components.minute = 0
+    return Calendar.current.date(from: components) ?? Date()
+}
