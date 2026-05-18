549275f23ffa1f3efd45d27d15fdbe3090624ce8
macMini <diegopandolfo06@gmail.com>
2026-05-18 10:05:13 +0200

    Add Color hex utilities and design tokens

---
+++ SleepWell/Utilities/Color+Hex.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3:
+            (r, g, b) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (r, g, b) = (int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (r, g, b) = (0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255
+        )
+    }
+
+    static let appBackground = Color(hex: "0b0d14")
+    static let appBackgroundEnd = Color(hex: "090b16")
+    static let accent = Color(hex: "4f6ef7")
+}
