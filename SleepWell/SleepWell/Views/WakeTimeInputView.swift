865393c5c453c5b452a3abd7d09b1b19fcb89fbe
macMini <diegopandolfo06@gmail.com>
2026-05-19 10:01:31 +0200

    Scale logo to 15% larger than original

--- SleepWell/SleepWell/Views/WakeTimeInputView.swift
+++ SleepWell/SleepWell/Views/WakeTimeInputView.swift
@@ -15,7 +15,7 @@ struct WakeTimeInputView: View {
                     Image("Logo")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 84, height: 101)
+                        .frame(width: 92, height: 110)
 
                     Text("SleepWell")
                         .font(.system(size: 12, weight: .semibold))
